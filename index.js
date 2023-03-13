b3dd1435568849e5f0aa9433504a52827a53fe7c
waltersimekha <waltersimekha@gmail.com>
2023-03-13 21:20:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+const currentUser = 'walterSimekha';
+let welcomeMessage = "Welcome to Flatbook, "
+
+//Using string concatenation
+ welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+// using template literals
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//Changing to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//slicing
+const firstInitial = currentUser.slice(0,1);
+// const restOfName = currentUser.slice
+const shortGreeting = `Welcome, ${firstInitial}!`;
